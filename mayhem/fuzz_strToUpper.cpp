56ded51710af58b029871476f88f690b73adab64
Raj Shah <rnshah9@asu.edu>
2022-12-23 05:22:06 +0000

    Fix build

--- mayhem/fuzz_strToUpper.cpp
+++ mayhem/fuzz_strToUpper.cpp
@@ -3,14 +3,14 @@
 #include <climits>
 
 #include <fuzzer/FuzzedDataProvider.h>
-#include "CharInfo.h"
+#include "slang/util/String.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     FuzzedDataProvider provider(data, size);
-    char c = provider.ConsumeIntegral<char>();
+    std::string str = provider.ConsumeRandomLengthString();
 
-    slang::utf8SeqBytes(c);
+    slang::strToUpper(str);
 
     return 0;
 }
